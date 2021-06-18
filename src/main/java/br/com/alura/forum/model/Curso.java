503bcaf0e3d7dec47f2df1cff5b57bab67088e62
jjeanjacques10 <jjean.jacques10@gmail.com>
2021-06-18 13:13:58 -0300

    delete duplicate constructors

--- src/main/java/br/com/alura/forum/model/Curso.java
+++ src/main/java/br/com/alura/forum/model/Curso.java
@@ -22,11 +22,6 @@ public class Curso {
     private String nome;
     private String categoria;
 
-    public Curso(String nome, String categoria) {
-        this.nome = nome;
-        this.categoria = categoria;
-    }
-
     @Override
     public int hashCode() {
         final int prime = 31;
--- src/main/java/br/com/alura/forum/model/Topico.java
+++ src/main/java/br/com/alura/forum/model/Topico.java
@@ -32,12 +32,6 @@ public class Topico {
     @OneToMany(mappedBy = "topico")
     private List<Resposta> respostas = new ArrayList<>();
 
-    public Topico(String titulo, String mensagem, Curso curso) {
-        this.titulo = titulo;
-        this.mensagem = mensagem;
-        this.curso = curso;
-    }
-
     @Override
     public int hashCode() {
         final int prime = 31;
